7dad1f46404147c4040f866f6067a2b818289941
Nicolás Mendoza <nmendoza@fundacionintegra.org>
2026-04-13 12:50:14 +0200

    add force dynamics en restricciones page

--- app/(protected)/planificacion/restricciones/page.tsx
+++ app/(protected)/planificacion/restricciones/page.tsx
@@ -1,6 +1,8 @@
 import { RestriccionesPageClient } from "@/components/planificacion/RestriccionesPageClient";
 import { obtenerRestricciones, obtenerEmpresas, obtenerTalleres } from "@/lib/api";
 
+export const dynamic = 'force-dynamic';
+
 export default async function RestriccionesPage() {
   const [empresas, restricciones, talleres] = await Promise.all([
     obtenerEmpresas(),
